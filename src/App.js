597f2ed3fbfc173117cc2f3c4a89af62feb53948
Star Harden <starmillerharden@gmail.com>
2020-04-13 23:01:59 -0500

    move audio to new file, add useLayoutEffect, WIP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import './sounds/audio';
 import './App.css';
 
 // useRef: a react hook that gives you access to any DOM element
@@ -13,11 +14,25 @@ import './App.css';
 
 function App() {
 
+  const keysReference = useRef(null);
+  const key = useRef(null);
+  const audio = useRef(null);
+
+// this will be called when React DOM is rendered
+  useLayoutEffect(
+    () => {
+      const keys = document.querySelectorAll('.key');
+      console.log('keys grabbed using useLayoutEffect', keys);
+      keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+      window.addEventListener('keydown',playSound);
+    },
+  )
+    
   function playSound(e) {
     // Goal: take e.keyCode's value, 
       //then find the audio element that has data-key attribute with the same value
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[dataKey="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[dataKey="${e.keyCode}"]`);
     if (!audio) return; // stop the function and exit
 
     audio.currentTime = 0; // rewind to the begining
@@ -26,81 +41,61 @@ function App() {
   }
 
   function removeTransition(e) {
-    if (e.propertyName !== 'transform') return // skip it if it's not a transorm
+    
+  if (e.propertyName !== 'transform') return // skip it if it's not a transorm
    this.classList.current.remove('playing');
-  }
-
+  
   const keys = document.querySelectorAll('.key');
-  console.log('keys grabbed using querySelectAll', keys);
+  // console.log('keys grabbed using querySelectAll', keys);
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown',playSound);
 
-
-  const keysReference = useRef(null);
   const keysDivElement = keysReference.current === null ? { children: [] } : keysReference.current;
   keysDivElement.children.forEach(key => key.addEventListener('transitionend', removeTransition))
+  
 
-
-  // this will be called when React DOM is rendered
-  useEffect(
-    () => {
-      const keys = document.querySelectorAll('.key');
-      console.log('keys grabbed using useEffect', keys);
-      keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-      window.addEventListener('keydown',playSound);
-    }
-  )
- 
+  } 
    return (
     <>
       <div className="keys" ref={keysReference}>
-        <div data-key="65" className="key">
+        <div dataKey="65" className="key">
           <kbd>A</kbd>
           <span className="sound">clap</span>
         </div>
-        <div data-key="83" className="key">
+        <div dataKey="83" className="key">
           <kbd>S</kbd>
           <span className="sound">hihat</span>
         </div>
-        <div data-key="68" className="key">
+        <div dataKey="68" className="key">
           <kbd>D</kbd>
           <span className="sound">kick</span>
         </div>
-        <div data-key="70" className="key">
+        <div dataKey="70" className="key">
           <kbd>F</kbd>
           <span className="sound">openhat</span>
         </div>
-        <div data-key="71" className="key">
+        <div dataKey="71" className="key">
           <kbd>G</kbd>
           <span className="sound">boom</span>
         </div>
-        <div data-key="72" className="key">
+        <div dataKey="72" className="key">
           <kbd>H</kbd>
           <span className="sound">ride</span>
         </div>
-        <div data-key="74" className="key">
+        <div dataKey="74" className="key">
           <kbd>J</kbd>
           <span className="sound">snare</span>
         </div>
-        <div data-key="75" className="key">
+        <div dataKey="75" className="key">
           <kbd>K</kbd>
           <span className="sound">tom</span>
         </div>
-        <div data-key="76" className="key">
+        <div dataKey="76" className="key">
           <kbd>L</kbd>
           <span className="sound">tink</span>
         </div>
       </div>
 
-      <audio data-key="65" src="sounds/clap.wav"></audio>
-      <audio data-key="83" src="sounds/hihat.wav"></audio>
-      <audio data-key="68" src="sounds/kick.wav"></audio>
-      <audio data-key="70" src="sounds/openhat.wav"></audio>
-      <audio data-key="71" src="sounds/boom.wav"></audio>
-      <audio data-key="72" src="sounds/ride.wav"></audio>
-      <audio data-key="74" src="sounds/snare.wav"></audio>
-      <audio data-key="75" src="sounds/tom.wav"></audio>
-      <audio data-key="76" src="sounds/tink.wav"></audio>
     </>
 
   );
--- src/sounds/audio.js
+++ src/sounds/audio.js
@@ -1,16 +1,58 @@
-{ 
-      <div>
-      
-            <audio data-key="65" src="sounds/clap.wav"></audio>
-            <audio data-key="83" src="sounds/hihat.wav"></audio>
-            <audio data-key="68" src="sounds/kick.wav"></audio>
-            <audio data-key="70" src="sounds/openhat.wav"></audio>
-            <audio data-key="71" src="sounds/boom.wav"></audio>
-            <audio data-key="72" src="sounds/ride.wav"></audio>
-            <audio data-key="74" src="sounds/snare.wav"></audio>
-            <audio data-key="75" src="sounds/tom.wav"></audio>
-            <audio data-key="76" src="sounds/tink.wav"></audio>
-
-      </div> 
-
-}
+export default [
+      {
+        dataKey: 65,
+        label: 'clap',
+        file: 'sounds/clap.wav',
+      },
+      {
+        dataKey: 83,
+        label: 'hihat',
+        file: 'sounds/hihat.wav',
+      },
+      {
+        dataKey: 68,
+        label: 'kick',
+        file: 'sounds/kick.wav',
+      },
+      {
+        dataKey: 70,
+        label: 'openhat',
+        file: 'sounds/openhat.wav',
+      },
+      {
+        dataKey: 71,
+        label: 'boom',
+        file: 'sounds/boom.wav',
+      },
+      {
+        dataKey: 72,
+        label: 'ride',
+        file: 'sounds/ride.wav',
+      },
+      {
+        dataKey: 74,
+        label: 'snare',
+        file: 'sounds/snare.wav',
+      },
+      {
+        dataKey: 75,
+        label: 'tom',
+        file: 'sounds/tom.wav',
+      },
+      {
+        dataKey: 76,
+        label: 'tink',
+        file: 'sounds/tink.wav',
+      },
+    ];
+      // <div className="keys" ref={keysReference}>
+      //       <audio dataKey="65" src="sounds/clap.wav"></audio>
+      //       <audio dataKey="83" src="sounds/hihat.wav"></audio>
+      //       <audio dataKey="68" src="sounds/kick.wav"></audio>
+      //       <audio dataKey="70" src="sounds/openhat.wav"></audio>
+      //       <audio dataKey="71" src="sounds/boom.wav"></audio>
+      //       <audio dataKey="72" src="sounds/ride.wav"></audio>
+      //       <audio dataKey="74" src="sounds/snare.wav"></audio>
+      //       <audio dataKey="75" src="sounds/tom.wav"></audio>
+      //       <audio dataKey="76" src="sounds/tink.wav"></audio>
+      // </div>
